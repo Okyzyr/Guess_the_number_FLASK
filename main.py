27dbadf570f9f26844e744fbcbac9ca26e65bb62
tenteges <kcukierski@gmail.com>
2021-03-28 20:18:37 +0200

    working on answers in form2

--- main.py
+++ main.py
@@ -20,13 +20,27 @@ def guess_number():
     </form>
     </p>
     """
-    too_big = """
-    
-    """
-    too_small = """
-    
-    """
+    answ_list = """
+        <select type="select" name ="answer">
+            <option>Too big</option>
+            <option>Too small</option>
+            <option>Correct</option>
+        </select>
+        <input type="submit" value="Oblicz">
+        """
 
+
+    odp = """
+    <p>
+    <input type="button" name="big" value="Too big">
+    <input type="button" name="small" value="Too small">
+    <input type="button" name="hit" value="Correct">
+
+    </p>
+    """
+    answer = request.args.get('big')
+    # to_small = request.args.get('small')
+    # correct = request.args.get('hit')
     a = request.args.get('a', 0)
     if a != None:
         a = int(a)
@@ -34,12 +48,9 @@ def guess_number():
             out_of_range = f"{a} is out of range. Try again."
             return rules + form + out_of_range
         else:
-            answer = a
-            while tried < 10:
-
-
-
-                return rules + str(a)
+            answer = str(request.args.get("answer"," "))
+            show_number = f"<p>Your number is {a}</p>"
+            return rules + show_number + answer + answ_list + answer
 
     else:
         return rules + form + "Podaj liczbę z zakresu"
